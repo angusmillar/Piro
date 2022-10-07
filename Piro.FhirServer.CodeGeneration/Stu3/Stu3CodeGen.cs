b7462ae785a9878c590f9c3c051538d02ff80e67
angusmillar <angusbmillar@gmail.com>
2022-10-07 14:56:53 +1000

    Clean up unused usings

---
+++ Piro.FhirServer.CodeGeneration/Stu3/Stu3CodeGen.cs
@@ -0,0 +1,20 @@
+﻿extern alias Stu3;
+using Stu3Model = Stu3.Hl7.Fhir.Model;
+using System.Collections.Generic;
+
+namespace Piro.FhirServer.CodeGeneration.Stu3
+{
+  public static class Stu3CodeGen
+  {
+    public static List<string> GetResourceNameList()
+    {
+      return Stu3Model.ModelInfo.SupportedResources;
+    }
+    public static Stu3Model.Bundle GetSearchParameterBundle()
+    {
+      return new SearchParametersBundleLoader().Load();
+    }
+  }
+}
+
+
